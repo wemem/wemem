1519463e994226def7a0237bd2288c9c9c83b0a1
ChildWang <wangchengdu1992@gmail.com>
2024-08-19 11:28:38 +0800

    feat(component):修改主题色

--- packages/frontend/component/src/theme/theme.css.ts
+++ packages/frontend/component/src/theme/theme.css.ts
@@ -1,8 +1,4 @@
-import {
-  cssVar,
-  darkCssVariables,
-  lightCssVariables,
-} from '@toeverything/theme';
+import { cssVar } from '@toeverything/theme';
 import { globalStyle } from '@vanilla-extract/css';
 
 globalStyle('body', {
@@ -11,26 +7,29 @@ globalStyle('body', {
   fontSize: cssVar('fontBase'),
 });
 
-globalStyle('html', {
-  vars: {
-    ...lightCssVariables,
-    '--affine-brand-color': 'hsl(240 5.9% 10%)',
-    '--affine-primary-color': 'hsl(240 5.9% 10%)',
-    '--affine-active-shadow': '#000',
-  },
-});
-globalStyle('html[data-theme="dark"]', {
-  vars: {
-    ...darkCssVariables,
-    '--affine-brand-color': 'hsl(0 0% 98%)',
-    '--affine-primary-color': 'hsl(0 0% 98%)',
-    '--affine-toggle-circle-background-color': '#000',
-    '--affine-pure-white': '#000',
-    '--affine-active-shadow': 'rgba(0,0,0,.05)',
-  },
-});
 if (process.env.NODE_ENV === 'development') {
   globalStyle('.undefined', {
     border: '5px solid red !important',
   });
 }
+
+globalStyle('[data-theme="light"]', {
+  vars: {
+    '--affine-brand-color': 'hsl(240 5.9% 10%)',
+    '--affine-primary-color': 'hsl(240 5.9% 10%)',
+    '--affine-active-shadow': '#000',
+    '--affine-button-primary': 'hsl(240 5.9% 10%)',
+  },
+});
+
+globalStyle('[data-theme="dark"]', {
+  vars: {
+    '--affine-brand-color': 'hsl(0 0% 98%)',
+    '--affine-primary-color': 'hsl(0 0% 98%)',
+    '--affine-toggle-circle-background-color': '#000',
+    '--affine-pure-white': '#000',
+    '--affine-active-shadow': 'rgba(0,0,0,.05)',
+    '--affine-button-primary': 'hsl(0 0% 98%)',
+    '--affine-button-pureWhiteText': 'hsl(240 5.9% 10%)',
+  },
+});
